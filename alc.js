55b9b3c3934f329898f1d6456c84094a5f30e38c
Asgurama <sadiqgurama1@gmail.com>
2024-11-04 08:13:28 +0100

    Add files via upload

---
+++ alc.js
@@ -0,0 +1,300 @@
+const alcData = {
+    "Abia": [
+        "Innovation Growth Hub",
+        "RAD5 Tech Hub",
+        "Yes Catholic Hangout",
+        "Abia Tech Hub",
+        "SoftIcu Technology"
+    ],
+    "Adamawa": [
+        "Leyomart Ltd",
+        "BCH Hub",
+        "ACIT American University of Nigeria, Yola, Adamawa State",
+        "North East Humanitarian Hub"
+    ],
+    "Akwa Ibom": [
+        "Wedigraf Technologies Ltd",
+        "Roothub Accelerator Systems Ltd",
+        "Laytec Consulting Ltd",
+        "Start Innovation Hub",
+        "Early Founder Innovation Hub"
+    ],
+    "Anambra": [
+        "LM Tech Hub",
+        "Integral Development Konsult Learning Centre",
+        "Innovation Growth Hub"
+    ],
+    "Bauchi": [
+        "A+ Computer Training Technology Limited" ,
+        "Taibah Innovation Hub" ,
+        "FEDERAL MINISTRY OF LABOUR AND EMPLOYMENT SKILLS UPGRADING AND VOCATIONAL TRAINING CENTRE" ,
+          "ATBU CISCO Academy",
+          "Zee Tech Foundation"
+    ],
+    
+    "Bayelsa":[
+"Olotu Square",
+"Bayelsa Tech Hub",
+"PSP Analytics"] ,
+
+"Benue":[
+"MICOTECH ICT solutions and services",
+"Wellington Workplace LTD",
+"Vefiken Nigeria Ltd",
+"Sky Hub Nigeria",
+"Central Soft Enterprise"],
+
+"Borno":[
+"Fathi-Tech Solutions LTD",
+"CIA Tech Africa",
+"Development Hub",
+"University of Maiduguri",
+"Co-Development Hub"],
+
+"Cross river":[
+"PSP Analytics",
+"Guru Innovation Hub (An initiative of the Guru Leadership Foundation)- Calabar Municipality",
+"Dahel Consultants and Techies - Calabar Municipality",
+"Guru Innovation Hub (An initiative of the Guru Leadership Foundation)- Calabar South",
+"Dahel Consultants and Techies - Calabar South"],
+
+"Delta":[
+
+"Great Minds in Tech Foundation - Oshimili",
+"Great Minds in Tech Foundation - Ughelli North",
+"Romanspage Global Services Nigeria Limited",
+"Waffi Hub Ltd",
+"Great Minds in Tech Foundation - Warri South"],
+
+"Ebonyi":[
+"Spella ICT Hub",
+"JEEF Tech Hub",
+"Elyon Global Empire Limited",
+"Elisco Resort Limited",
+"Resnovus Consult Limited"],
+
+"Edo":[
+"Imose Technologies",
+"Romanspage Global Services Nigeria Limited",
+"Code Nation",
+"Decipher Innovate"],
+
+"Ekiti":[
+"T21 Consultancy Services Limited",
+"Kinplus Technologies Limited",
+"Purple Beeeducational services Limited"],
+
+"Enugu":[
+"Foretrust Digital Consulting Ltd",
+"Attueyi Coding Academy",
+"Genesys Tech Hub",
+"Integrated Elvee Services LTD"],
+
+"FCT Abuja":[
+"UNICCON Group of Companies Ltd",
+"Seedbuilders Innovation Hub",
+"Domineum Blockchain Solutions Limited",
+"Startup Arewa",
+"Gogetit Investments Limited",
+"OMNI Digital Media Institute",
+"Womenovate",
+"Fainajs Express Ltd"],
+
+"Gombe":[
+"Briatek Computer Nig. Ltd",
+"Agiletech Global Services",
+"Doopals Technologies Ltd"],
+
+"Imo":[
+"Julz Knightbridge Limited",
+"Federal University of Technology, Owerri",
+"Technology for Inspiration Initiative",
+"First Class Vocational Training Institute Limited",
+"Codeant Technology Hub"],
+
+"Jigawa":[
+"Voost Technologies Limited",
+"Startup Jigawa",
+"Blue Sapphire Hub",
+"Computing Research Accelaration Initiative",
+
+"Jigawa State Institute of Information Technology (JSIIT) - Kazaure",
+
+"Jigawa State Institute of Information Technology (JSIIT) - Kaugama"],
+
+"Kaduna":[
+"YandyTech Community Development Initiative",
+"3logoy Limited",
+"KadICT",
+"Young Innovators of Nigeria",
+"The Nexus work & Connect Limited",
+"Almara Hub",
+"Ihifix Technologies"],
+
+"Kano":[
+"Blue Sapphire Hub",
+"4U Talent Accelerator Limited",
+"Hikima Information System",
+"Kano State Polytechnic (NITDA CISCO A cademy)",
+"Starbench Limited",
+"ECM Hub",
+"Steminnovatorslab Limited",
+"Steamledge Limited",
+"Rosary Hub Enterprises",
+"Aisha Kwaku",
+"Engausa Hub.com",
+"Enovate Lab"],
+
+"Katsina":[
+"Lumilab & Tech Services Limited",
+"Kirkira Innovation Hub Nig Ltd",
+"Hassan Usman Katsina Polytechnic",
+"TEKx",
+"Collective Development (CODE)"],
+
+"Kebbi":[
+"Ikwaire Computing Services and Academy",
+"ABUTECH IT Hub",
+"Federal University Birnin Kebbi",
+"Ideation Oasis Hub"],
+
+"Kogi":[
+"Brainpals Code Academy",
+"Mentor-Box Foundation",
+"STARTECHONE Nigeria Limited"],
+
+"Kwara":[
+"Kuagi Resources",
+"MALhub Technology Nigeria Limited",
+"Kwara Tech Academy",
+"CYPHERDEVS"],
+
+"Lagos":[
+"IT Skills center - Agege",
+"Hommaston Limited - Alimosho",
+"Centre For Research in Enterprise and Action in Management (CREM Nigeria)",
+"LASU CBT 18",
+"Hommaston Limited - Amuwo Odofin",
+"Skillpaddy Technology",
+"Teesas Education Limited - Eti Osa",
+"Blue Sands ACADEMY",
+"IT Skills Center - Ifako Ijaye",
+"Teesas Education Limited - Ikeja",
+"SAIL Innovation Lab",
+"Premier Startups Limited (Premier Hub)",
+"ScholarX Technology - Lagos Mainland",
+"ScholarX Technology - Mushin",
+"Institute of Data Processing Management of Nigeria",
+"Centre for Research in Enterprise and Action in Management (CREM Nigeria)",
+"The Nest Innovation Hub",
+"Enov8 Tech Hub Limited (The Bulb Africa)",
+"Ingrid Academy"],
+
+"Nasarawa":[
+"Julz Knightbridge Limited",
+"02 Innovations Lab Nig. Ltd",
+"Cloudtech Connect"],
+
+"Niger":[
+"Pistis CISCO Networking Academy",
+"Paritie Innovation Hub",
+"Digital Dynamic Ltd",
+"Young Innovators of Nigeria"],
+
+"Ogun":[
+"TedPrime Hub - Abeokuta North",
+"Grazac Technologies Ltd",
+"TedPrime Hub - Abeokuta South",
+"720 Degree Innovation Hub",
+"Godsoft Systmes Ltd"],
+
+"Ondo":[
+"JIT Solutions",
+"CISCO Academy, FUTA",
+"N-Tech Information Systems",
+"CyberOceans Integrated Services",
+"W.Tech Software"],
+
+"Osun":[
+"FlexWork Nigeria Limited",
+"SYL Multimedia",
+"Prepper Learning Limited",
+"Classic Systems Infotech Ltd",
+"Brain Builder IT",
+"Founding Networks"],
+
+"Oyo":[
+"Highland College of Technology",
+"Educational Advancement Centre",
+"CodeSchool Africa",
+"HSProjects Technologies",
+"EduLight Consulting",
+"Foundation for Integrated Care - Ibadan South East",
+"Ibadan Digital Academy",
+"Foundation for Integrated Care - Ibadan South West",
+"SQI College of ICT",
+"ORX Solutions Limited"],
+
+"Plateau":[
+"Centre for Entrepreneurship Studies (UNIJOS)",
+"Sunnah Tech Lab",
+"Mainstream Excellent Hub",
+"nHub Incubator and Accelerator",
+"Black Innovations Africa"],
+
+"Rivers":[
+"KHc Trades & Technologies LTD",
+"PAN African Professional Studies",
+"CAD Consulting Limited",
+"Olotusquare",
+"Wedigraf Technologies Limited",
+"Quantum Business School - Obio/Akpor",
+"Quantum Business School - Portharcourt",
+"Entrepreneurship & Innovation Centre Limited"],
+
+"Sokoto":[
+"Sustainable Impact Hub",
+"Amtec Global Consulting Nig, Ltd",
+"Northwest University",
+"UDUS Digital Learning Center Ltd (Prome",
+"DDigital Solutions",
+"Startup Sokoto"],
+
+"Taraba":[
+
+"New Sky Technologies",
+"Taraba state University",
+"Trinidax Excel Limited",
+"Knots Technology Nig Ltd"],
+
+"Yobe":[
+"Ahlam IT and Network Solution",
+"Rhino Innovation Lab",
+"Yobe Young Generation Association",
+"Federal College of Education (Technical) Potiskum"],
+
+"Zamfara":[
+"Federal University Gusau",
+"Mai-Gus Software Technology (Maisoft)",
+"Ramlat Global Academy",
+"Startup Zamfara"]
+};
+
+function renderALC() {
+    const container = document.getElementById('alc-container');
+    for (const state in alcData) {
+        const stateHeader = document.createElement('h2');
+        stateHeader.textContent = state;
+        container.appendChild(stateHeader);
+
+        const list = document.createElement('ul');
+        alcData[state].forEach(hub => {
+            const listItem = document.createElement('li');
+            listItem.textContent = hub;
+            list.appendChild(listItem);
+        });
+        container.appendChild(list);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', renderALC);
